2bbe41cfffdef6860113b2923f56e431bce4cd45
neeeal <matira.nealbartonjames@gmail.com>
2023-09-08 07:44:00 +0800

    used mobilenetv3 large

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Sequential
+from tensorflow.keras.applications import MobileNetV3Large
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.losses import CategoricalCrossentropy
 import os
 
 ## ENVIRONMENT
 os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
 
-
-## GLOBAL VARIABLES 
-img_shape = (64, 64)
-channels = 3
-batch_size = 128
-classes=1081
-epochs=8
 
 ## DATA DIRECTORIES
 data_dir = "plantnet_300k"
@@ -21,9 +16,18 @@ train_dir = os.path.join(data_dir, "images_train")
 test_dir = os.path.join(data_dir, "images_test")
 val_dir = os.path.join(data_dir, "images_val")
 
+## GLOBAL VARIABLES 
+img_shape = (75, 75)
+channels = 3
+batch_size = 32
+classes=len(os.listdir(train_dir))-1
+epochs=16
+
+
 ## DATA GENERATORS
 train_generator = ImageDataGenerator(rescale=1./255,
                                      horizontal_flip=True,
+                                     vertical_flip=True,
                                      width_shift_range=0.2,
                                      height_shift_range=0.2,
                                     #  rotation_range=40,
@@ -52,21 +56,48 @@ val_generator = ImageDataGenerator(rescale=1./255).flow_from_directory(
                                                 )
 
 ## MODEL
+mobilenet = MobileNetV3Large(
+    input_shape=img_shape+(channels,),
+    alpha=1.0,
+    minimalistic=False,
+    include_top=False,
+    weights='imagenet',
+    input_tensor=None,
+    # classes=classes,
+    pooling=None,
+    dropout_rate=0.4,
+    classifier_activation='softmax',
+    include_preprocessing=True
+)
+
+
+print("LAYERS ",len(mobilenet.layers))
+for layer in mobilenet.layers[:-130]:
+    layer.trainable = False
+
 model = Sequential()
 
 ## FEATURE EXTRACTION
-model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=1, activation='relu', input_shape=img_shape+(channels,)))
-model.add(Conv2D(filters=64, kernel_size=(5, 5), strides=1, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=1, activation='relu', input_shape=img_shape+(channels,)))
+# model.add(Conv2D(filters=64, kernel_size=(5, 5), strides=1, activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dropout(0.4))
+# model.add(Dropout(0.4))
 
-model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=1, activation='relu'))
-model.add(Conv2D(filters=256, kernel_size=(5, 5), strides=1, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=1, activation='relu'))
+# model.add(Conv2D(filters=128, kernel_size=(5, 5), strides=1, activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(Dropout(0.4))
+
+# model.add(Conv2D(filters=256, kernel_size=(3, 3), strides=1, activation='relu'))
+# model.add(Conv2D(filters=256, kernel_size=(5, 5), strides=1, activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(mobilenet)
 
 ## DENSE LAYERS / HIDDEN LAYERS
-model.add(GlobalAveragePooling2D())
+model.add(Flatten())
+model.add(Dense(units=256, activation='relu'))
 model.add(Dense(units=256, activation='relu'))
 
 ## CLASSIFICATION LAYER
@@ -74,14 +105,16 @@ model.add(Dense(units=classes, activation='softmax'))
 
 
 ## COMPILING
-model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['acc'])
 model.summary()
 
 ## TRAINING
 history = model.fit(train_generator,
                     epochs=epochs,
-                    validation_data=val_generator,
+                    validation_data=test_generator,
                     verbose=1,
                     batch_size=batch_size
                     )
-
+model_dir = "plantnet_300K//models"
+model_version = len(os.listdir(model_dir))
+model.save(f'{model_dir}//modelV{model_version}.h5')
