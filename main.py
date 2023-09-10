91db694b01ef2d2d91b88e7292c46f3040541ad8
neeeal <matira.nealbartonjames@gmail.com>
2023-09-10 12:04:42 +0800

    changed from mobilenet to inception model. used reduce lr ad early stopping callbacks

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Sequential
-from tensorflow.keras.applications import MobileNetV3Large
+from tensorflow.keras.applications import MobileNetV3Large, MobileNetV3Small, MobileNetV2
+from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 import os
 
 ## ENVIRONMENT
@@ -17,22 +19,22 @@ test_dir = os.path.join(data_dir, "images_test")
 val_dir = os.path.join(data_dir, "images_val")
 
 ## GLOBAL VARIABLES 
-img_shape = (75, 75)
+img_shape = (128,128)
 channels = 3
-batch_size = 32
+batch_size = 8
 classes=len(os.listdir(train_dir))-1
-epochs=16
+epochs=64
 
 
 ## DATA GENERATORS
 train_generator = ImageDataGenerator(rescale=1./255,
                                      horizontal_flip=True,
                                      vertical_flip=True,
-                                     width_shift_range=0.2,
-                                     height_shift_range=0.2,
-                                    #  rotation_range=40,
-                                    #  shear_range=0.2,
-                                    #  zoom_range=0.2
+                                     width_shift_range=0.4,
+                                     height_shift_range=0.4,
+                                     rotation_range=40,
+                                     shear_range=0.2,
+                                     zoom_range=0.2
                                      ).flow_from_directory(
                                                     train_dir,
                                                     target_size=img_shape,
@@ -55,25 +57,21 @@ val_generator = ImageDataGenerator(rescale=1./255).flow_from_directory(
                                                     shuffle=True
                                                 )
 
-## MODEL
-mobilenet = MobileNetV3Large(
+## TRANSFER LEARNING MODEL
+transfer_model = InceptionV3(
     input_shape=img_shape+(channels,),
-    alpha=1.0,
-    minimalistic=False,
     include_top=False,
     weights='imagenet',
-    input_tensor=None,
     # classes=classes,
-    pooling=None,
-    dropout_rate=0.4,
-    classifier_activation='softmax',
-    include_preprocessing=True
+    # dropout_rate=0.4,
+    # classifier_activation='softmax',
+    # include_preprocessing=True
 )
 
 
-print("LAYERS ",len(mobilenet.layers))
-for layer in mobilenet.layers[:-130]:
-    layer.trainable = False
+print("LAYERS ",len(transfer_model.layers))
+# for layer in mobilenet.layers[:-130]:
+#     layer.trainable = False
 
 model = Sequential()
 
@@ -93,28 +91,33 @@ model = Sequential()
 # model.add(Conv2D(filters=256, kernel_size=(3, 3), strides=1, activation='relu'))
 # model.add(Conv2D(filters=256, kernel_size=(5, 5), strides=1, activation='relu'))
 # model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(mobilenet)
+model.add(transfer_model)
 
 ## DENSE LAYERS / HIDDEN LAYERS
-model.add(Flatten())
-model.add(Dense(units=256, activation='relu'))
-model.add(Dense(units=256, activation='relu'))
+model.add(GlobalAveragePooling2D())
+model.add(Dropout(0.3))
+# model.add(Dense(units=256, activion='relu'))
+# model.add(Dense(units=256, activatation='relu'))
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                              patience=3, min_lr=1e-7)
+es = EarlyStopping(monitor='loss', patience=5)
 
 ## CLASSIFICATION LAYER
 model.add(Dense(units=classes, activation='softmax'))
 
 
 ## COMPILING
-model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer=Adam(1e-4), metrics=['acc'])
 model.summary()
 
 ## TRAINING
 history = model.fit(train_generator,
                     epochs=epochs,
-                    validation_data=test_generator,
+                    validation_data=val_generator,
                     verbose=1,
                     batch_size=batch_size
-                    )
+                    callbacks=[es, reduce_lr])
 model_dir = "plantnet_300K//models"
 model_version = len(os.listdir(model_dir))
 model.save(f'{model_dir}//modelV{model_version}.h5')
